18ea725578a70507669e41be0b03a219f29a31eb
raybanboy <53579715+raybanboy@users.noreply.github.com>
2021-06-11 18:02:12 +0200

    Add files via upload

---
+++ character_controller.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_controller : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private Vector2 velocity;
+
+    private float moveInput;
+    private bool isGrounded;
+
+    public float speed;
+    public Transform feetPos;
+    public float checkRadius;
+    public float jumpForce;
+    public LayerMask whatIsGround;
+
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
+
+    // Start is called before the first frame update
+   void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if(moveInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if(moveInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 100, 0);
+        }
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+
+            if(Input.GetKeyUp(KeyCode.Space))
+            {
+                isJumping = false;
+            }
+        }
+    }
+   }
