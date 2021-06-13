3136ad8ae70bcc03783c7f04d2281e1aad2ed25f
raybanboy <53579715+raybanboy@users.noreply.github.com>
2021-06-13 21:09:50 +0200

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public GameObject panel;
+    public Vector2 lastMove;
+    public float attackingTime;
+    public string startPoint;
+
+    private Animator anim;
+    private Rigidbody2D myBody;
+    private bool playerMoving;
+    private static bool playerrExist;
+    private bool attacking;
+    private float attackTimeCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        myBody = GetComponent<Rigidbody2D>();
+
+        if(!playerrExist)
+        {
+            playerrExist = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMoving = false;
+
+        if(!attacking)
+        {
+            if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+            {
+                myBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myBody.velocity.y);
+                playerMoving = true;
+                lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+            }
+            if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+            {
+                myBody.velocity = new Vector2(myBody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+                playerMoving = true;
+                lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+            }
+            if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+            {
+                myBody.velocity = new Vector2(0f, myBody.velocity.y);
+            }
+            if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+            {
+                myBody.velocity = new Vector2(myBody.velocity.x, 0f);
+            }
+        }
+        if (attackTimeCounter > 0)
+            attackTimeCounter -= Time.deltaTime;
+
+        if(attackTimeCounter <= 0)
+        {
+            attacking = false;
+            anim.SetBool("PlayerAttacking", false);
+
+        }
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveX", lastMove.x);
+
+    }
+}
